a1a0ffabef648224472e8888f68a8ac42770b1c7
anujjha101296-lang <anujjha.101296@gmail.com>
2026-09-09 20:38:48 +0530

    feat(science-runtime): expose research loop API

--- axiom/science_runtime/__init__.py
+++ axiom/science_runtime/__init__.py
@@ -8,12 +8,17 @@ from .benchmark import run_lorenz_reference_benchmark
 from .critic import Critique, critique_evidence
 from .lorenz import run_lorenz_experiment
 from .models import ExperimentRecord, LorenzResult
+from .research_loop import ResearchQuestion, ResearchRun, ResearchStage, run_research
 
 __all__ = [
     "Critique",
     "ExperimentRecord",
     "LorenzResult",
+    "ResearchQuestion",
+    "ResearchRun",
+    "ResearchStage",
     "critique_evidence",
     "run_lorenz_experiment",
     "run_lorenz_reference_benchmark",
+    "run_research",
 ]
